780dadd916db8b5acb8dc675019facfd25b223bd
Rémi Del Medico <remi.del-medico@etu.univ-grenoble-alpes.fr>
2023-02-21 14:49:27 +0100

    Recherche cle

--- A234/a234.c
+++ A234/a234.c
@@ -71,14 +71,31 @@ int CleMin (Arbre234 a)
 
 }
 
-Arbre234 RechercherCle (Arbre234 a, int cle)
-{
-  /*
-     rechercher si la cle a est presente dans
-     l'arbre a. Si oui, retourne le noeud ou se trouve la cle.
-  */
+Arbre234 RechercherCle (Arbre234 a, int cle) { //42 lignes
+  if (a == NULL || a->t == 0)
+    return NULL;
 
-  return NULL ;
+  if (cle == a->cles[1])
+    return a;
+
+  if (a->cles[1] > cle) {
+    if (a->t > 2) {
+      if (cle == a->cles[0]) {
+        return a;
+      }
+      return a->cles[0] > cle ? RechercherCle(a->fils[0], cle) : RechercherCle(a->fils[1], cle);
+    } else {
+      return RechercherCle(a->fils[1], cle);
+    }
+  } else { 
+    if (a->t > 3) {
+      if (a->cles[2] == cle) {
+        return a;
+      }
+      return a->cles[2] < cle ? RechercherCle(a->fils[3], cle) : RechercherCle(a->fils[2], cle);
+    }
+  }
+  return RechercherCle(a->fils[2], cle);
 }
 
 void AnalyseStructureArbre (Arbre234 a, int *feuilles, int *noeud2, int *noeud3, int *noeud4)
@@ -165,6 +182,8 @@ int main (int argc, char **argv)
     printf("MIN : %d\n", CleMin(a));
 
     printf("MAX : %d\n", CleMax(a));
+
+    afficher_arbre (RechercherCle(a, 82), 0) ;
 
     printf ("==== Hauteur arbre ====\n") ;
 
